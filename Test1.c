696e57dac5a4d56553f12cd1a8104c2cc44d5a32
asifulcse8 <105233622+asifulcse8@users.noreply.github.com>
2022-05-24 10:41:46 -0700

    Add files via upload

---
+++ Test1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a =10, b=15, add,sub,multi,div;
+    add = a + b;
+    sub = b - a;
+    multi = a * b;
+
+    printf("Hello World!!!\n");
+    printf("Total is : %d %d %d\n",add,sub,multi);
+
+}
